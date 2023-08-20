a0d5d039131622c917ecbe3bf76464bb732b535e
Semogs <semi.moreno1@gmail.com>
2023-08-20 20:06:02 +0300

    added cors changes

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const serverPort = process.env.PORT || 9733;
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
