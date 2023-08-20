41521b9690dae905f14decaed582d8efd7e8eb61
Semogs <semi.moreno1@gmail.com>
2023-08-20 20:03:12 +0300

    changed cors details

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require('express');
 require('dotenv').config();
-const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const serverPort = process.env.PORT || 9733;
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-
-  next();
-});
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
