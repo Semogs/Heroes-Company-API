a79186e4fbaf602673cef21263b0848fa14e2790
Semogs <semi.moreno1@gmail.com>
2023-08-20 20:24:09 +0300

    changed cors policy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const serverPort = process.env.PORT || 9733;
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://semogs.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
 
   next();
